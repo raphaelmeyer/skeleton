05df210b9cf7866dcfd7c4f90f98f8b8f7a6faa5
Raphael Meyer <raphael.meyer@bbv.ch>
2015-08-19 08:35:50 +0200

    use environment script in all scripts

--- acceptance-test-host.sh
+++ acceptance-test-host.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-HOST=0.1.0
-
 PROJECT_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SRC_HOST=${PROJECT_ROOT}/source
-SRC_GUEST=/home/user/src
 
-docker run -d -t --volumes-from host-workspace -v ${SRC_HOST}:${SRC_GUEST}:ro --expose 3902 --name wire-server host-gcc:${HOST} /workspace/acceptance-test-host/acceptance-test-host
+source ${PROJECT_ROOT}/env-host.sh
 
-docker run --rm -t --volumes-from host-workspace -v ${SRC_HOST}:${SRC_GUEST}:ro --link wire-server -w ${SRC_GUEST} host-gcc:${HOST} cucumber
+docker run -d -t --expose 3902 --name wire-server ${VOLUMES} ${CONTAINER} acceptance-test-host/acceptance-test-host
+docker run --rm -t --link wire-server -w ${SRC_GUEST} ${VOLUMES} ${CONTAINER} cucumber
 
 docker rm wire-server
 
