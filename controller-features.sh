24396dbd98a639dc33aa8cf5d6d6ef241ab0d7e7
raphael meyer <raphael.akira.meyer@gmail.com>
2016-02-21 14:17:51 +0100

    Move controller features to subfolder

--- controller-features.sh
+++ controller-features.sh
@@ -8,7 +8,7 @@ NAME=wire-server
 source ${PROJECT_ROOT}/env-amd64.sh
 
 docker run -d -t --expose 3902 --name ${NAME} ${VOLUMES} ${CONTAINER} ${WIRE_SERVER}
-docker run --rm -t --link ${NAME} -w ${SRC_GUEST} ${VOLUMES} ${CONTAINER} cucumber $@
+docker run --rm -t --link ${NAME} -w ${SRC_GUEST}/controller ${VOLUMES} ${CONTAINER} cucumber $@
 
 docker rm ${NAME}
 
