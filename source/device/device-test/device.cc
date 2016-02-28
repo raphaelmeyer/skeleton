338bc02fa8dc80519117c443c33e7adc1c1d402b
raphael meyer <raphael.akira.meyer@gmail.com>
2016-02-28 16:45:59 +0100

    Split test

--- source/device/device-test/device.cc
+++ source/device/device-test/device.cc
@@ -9,17 +9,24 @@ uint8_t PINB = 0;
 namespace
 {
 
-TEST(An_input_gpio, returns_its_pin_signal_state)
+TEST(An_input_gpio, returns_high_when_its_pin_is_set)
 {
   Gpio gpio;
   Gpio_init(&gpio, Port_B, Pin_3);
   Gpio_set_direction((IGpio *)&gpio, Direction_Input);
 
-  PINB = 0x00;
-  ASSERT_THAT(Gpio_get_signal((IGpio *)&gpio), Eq(Signal_Low));
-
-  PINB = 0x01;
+  PINB = (1 << 3);
   ASSERT_THAT(Gpio_get_signal((IGpio *)&gpio), Eq(Signal_High));
+}
+
+TEST(An_input_gpio, returns_low_when_its_pin_is_not_set)
+{
+  Gpio gpio;
+  Gpio_init(&gpio, Port_B, Pin_3);
+  Gpio_set_direction((IGpio *)&gpio, Direction_Input);
+
+  PINB = 0;
+  ASSERT_THAT(Gpio_get_signal((IGpio *)&gpio), Eq(Signal_Low));
 }
 
 TEST(A_gpio, returns_undefined_if_not_configured_as_input)
