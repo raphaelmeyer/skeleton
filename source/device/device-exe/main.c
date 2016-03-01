cf18af90260924eebc74ab7b844b692e5f67d438
raphael meyer <raphael.akira.meyer@gmail.com>
2016-03-01 18:57:52 +0100

    Add endless loop to main

--- source/device/device-exe/main.c
+++ source/device/device-exe/main.c
@@ -2,13 +2,17 @@
 #include <device/gpio.h>
 #include <device/pwm.h>
 
+#include <stdbool.h>
+
 struct Device device;
 struct Pwm bell;
 struct Gpio button;
 
 int main()
 {
-  Device_init(&device, (struct IPwm *)&bell, (struct IGpio *)&button);
+  Device_init(&device, (struct IPwm *)&bell, &button);
   Device_start(&device);
+
+  while(true);
 }
 
