bc3a1cd5647b1eb3e779a65a5592f7b62690aeec
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-09 07:54:36 +0100

    Test skeleton for ringing bell for a certain time only.

---
+++ source/device/device-lib/include/device/timer.h
@@ -0,0 +1,19 @@
+#ifndef SKELETON_TIMER_H
+#define SKELETON_TIMER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+struct ITimer
+{
+
+};
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif //SKELETON_TIMER_H
--- source/device/device-test/device.cc
+++ source/device/device-test/device.cc
@@ -3,6 +3,7 @@
 #include <device/device.h>
 #include <device/gpio.h>
 #include <device/pwm.h>
+#include <device/timer.h>
 
 #include <avr/io.h>
 
@@ -23,6 +24,28 @@ struct PwmStub
   }
 };
 
+struct TimerSpy
+{
+  enum State {
+    Idle, Running, Expired
+  };
+
+  ITimer interface;
+  State state;
+
+  static void start(ITimer * base) {
+    TimerSpy * self = (TimerSpy *)base;
+    self->state = Running;
+  }
+  static void stop(ITimer * base) {
+    TimerSpy * self = (TimerSpy *)base;
+    self->state = Idle;
+  }
+  static bool expired(ITimer * base) {
+    TimerSpy * self = (TimerSpy *)base;
+    return (self->state == Expired);
+  }
+};
 
 TEST(The_device, configures_the_button_pin_as_an_input)
 {
@@ -51,6 +74,31 @@ TEST(The_device, turns_on_the_bell_pwm_when_the_button_signal_is_high)
   Device_loop(&testee);
 
   ASSERT_TRUE(bell.turned_on);
+}
+
+TEST(The_device, DISABLED_sets_a_timer_to_rings_the_bell_for_a_certain_time)
+{
+  Gpio button;
+  TimerSpy timer{{}, TimerSpy::Idle};
+  PwmStub bell{{PwmStub::on, nullptr}, false};
+  Device testee;
+
+  Gpio_init(&button, Port_C, Pin_2);
+  Device_init(&testee, (IPwm *)&bell, &button);
+
+  PINC = (1 << 2);
+  Device_loop(&testee);
+
+  ASSERT_THAT(timer.state, Eq(TimerSpy::Running));
+}
+
+TEST(The_device, DISABLED_turns_the_bell_off_after_the_timer_expires)
+{
+  //timer.state = TimerSpy::Expired;
+  //Device_loop(&testee);
+  //ASSERT_FALSE(bell.turned_on);
+
+  FAIL();
 }
 
 } // namespace
