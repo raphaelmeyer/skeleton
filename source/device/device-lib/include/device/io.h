cf41df62ce0229c2e93f0102649586bc97656fab
raphael meyer <raphael.akira.meyer@gmail.com>
2016-03-13 14:30:07 +0100

    Refactor

--- source/device/device-lib/include/device/io.h
+++ source/device/device-lib/include/device/io.h
@@ -8,4 +8,9 @@ static inline uint8_t bit(uint8_t bit)
   return (1 << bit);
 }
 
+static inline uint8_t set_bit(uint8_t volatile * port, uint8_t bit)
+{
+  *port |= (1 << bit);
+}
+
 #endif //SKELETON_IO_H
--- source/device/device-lib/pwm.c
+++ source/device/device-lib/pwm.c
@@ -1,8 +1,10 @@
 #include "device/pwm.h"
 
+#include "device/io.h"
+
 #include <avr/io.h>
 
 void Pwm_init(struct Pwm * self)
 {
-  DDRD |= (1 << 6);
+  set_bit(&DDRD, 6);
 }
