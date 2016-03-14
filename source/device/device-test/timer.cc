27289d28b9d692a4d77a5234c780b60867213b87
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-14 18:28:02 +0100

    Todo list for timer in tests

--- source/device/device-test/timer.cc
+++ source/device/device-test/timer.cc
@@ -19,4 +19,33 @@ TEST(A_timer, is_not_expired_after_initialisation)
   ASSERT_FALSE(expired);
 }
 
+TEST(A_timer, is_not_expired_after_start_until_xxx)
+{
+  // Timer_init()
+  // Timer_start(3);
+  // ASSERT_FALSE(Timer_expired())
+  // TickStub.notify()
+  // ASSERT_FALSE(Timer_expired())
+
+  FAIL();
+}
+
+TEST(A_timer, DISABLED_expires_after_the_given_number_of_milliseconds)
+{
+  // Timer_init()
+  // Timer_start(3);
+  // ASSERT_FALSE(Timer_expired())
+  // TickStub.notify()
+  // TickStub.notify()
+  // TickStub.notify()
+  // ASSERT_TRUE(Timer_expired())
+
+  FAIL();
+}
+
+TEST(A_timer, DISABLED_is_not_expired_if_it_was_stopped)
+{
+  FAIL();
+}
+
 } // namespace
