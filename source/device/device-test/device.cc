701cba2ef3f489f15cee82c57e44e68aa5afcf38
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-16 17:10:54 +0100

    Ringing time

--- source/device/device-test/device.cc
+++ source/device/device-test/device.cc
@@ -77,7 +77,7 @@ TEST_F(The_device, does_not_turn_on_pwm_again_when_the_bell_is_already_ringing)
   ASSERT_THAT(bell.called_on, Eq(1));
 }
 
-TEST_F(The_device, turns_the_bell_off_after_the_timer_expires)
+TEST_F(The_device, turns_the_bell_off_after_1000_milliseconds)
 {
   Gpio_init(&button, Port_C, Pin_2);
   Timer_init(&timer);
@@ -87,12 +87,16 @@ TEST_F(The_device, turns_the_bell_off_after_the_timer_expires)
   PINC = (1 << 2);
   Device_loop(&testee);
 
-  Timer_update(&timer);
-  Timer_update(&timer);
-  Timer_update(&timer);
+  uint32_t const ringing_time = 1000;
 
+  for(uint32_t tick = 0; tick < ringing_time - 1; ++tick) {
+    Timer_update(&timer);
+    Device_loop(&testee);
+  }
+  ASSERT_TRUE(bell.turned_on);
+
+  Timer_update(&timer);
   Device_loop(&testee);
-
   ASSERT_FALSE(bell.turned_on);
 }
 
