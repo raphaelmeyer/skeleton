34cc6e859763b04f7d5ea8af32078d215c7820f5
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-18 07:54:11 +0100

    Documenting test

--- source/device/device-test/system_tick.cc
+++ source/device/device-test/system_tick.cc
@@ -79,9 +79,18 @@ TEST(The_system_tick, notifies_all_registered_observers)
   ASSERT_THAT(another_spy.called, Eq(1));
 }
 
-TEST(The_system_tick, DISABLED_supports_up_to_two_observers)
+TEST(The_system_tick, supports_up_to_two_observers)
 {
-  FAIL();
+  Spy spy;
+  Spy another_spy;
+
+  SystemTick_init();
+  SystemTick_register(notify_spy, &spy);
+  SystemTick_register(notify_spy, &another_spy);
+
+  TIMER1_COMPA_vect();
+  ASSERT_THAT(spy.called, Eq(1));
+  ASSERT_THAT(another_spy.called, Eq(1));
 }
 
 TEST(The_system_tick, DISABLED_ignores_observers_after_two_observers_have_registered)
