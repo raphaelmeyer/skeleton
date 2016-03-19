04301127241a441c24debd8828f31ab4bc6c53d5
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-19 08:33:50 +0100

    I/O seams for feature

--- source/device/features/test_doubles/seams.cc
+++ source/device/features/test_doubles/seams.cc
@@ -1,4 +1,4 @@
-#include <cstdint>
+#include <avr/io.h>
 
 uint8_t volatile PINB;
 uint8_t volatile PORTB;
@@ -11,3 +11,10 @@ uint8_t volatile DDRC;
 uint8_t volatile PIND;
 uint8_t volatile PORTD;
 uint8_t volatile DDRD;
+
+uint8_t volatile TCCR1A;
+uint8_t volatile TCCR1B;
+uint16_t volatile OCR1A;
+
+uint8_t const CS10 = 0;
+uint8_t const WGM12 = 3;
