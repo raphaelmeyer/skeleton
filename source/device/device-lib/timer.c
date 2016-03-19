8d219e0822ee6f9ec04e56728b128690b2d76518
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-19 16:14:52 +0100

    Remove Timer_update

--- source/device/device-lib/timer.c
+++ source/device/device-lib/timer.c
@@ -2,11 +2,14 @@
 
 #include "device/system_tick.h"
 
-void Timer_update(struct Timer * self);
-
 static void update(void * argument) {
   struct Timer * self = (struct Timer *)argument;
-  Timer_update(self);
+  if(self->remaining > 0) {
+    --self->remaining;
+    if(self->remaining == 0) {
+      self->expired = true;
+    }
+  }
 }
 
 void Timer_init(struct Timer * self) {
@@ -33,13 +36,3 @@ void Timer_stop(struct Timer * self) {
 bool Timer_expired(struct Timer * self) {
   return self->expired;
 }
-
-void Timer_update(struct Timer * self)  {
-  if(self->remaining > 0) {
-    --self->remaining;
-    if(self->remaining == 0) {
-      self->expired = true;
-    }
-  }
-}
-
