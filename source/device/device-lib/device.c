7226d879396613807baee0829cf678c9de3b14b9
raphael meyer <raphael.akira.meyer@gmail.com>
2016-03-23 19:20:56 +0100

    Refactor

--- source/device/device-lib/device.c
+++ source/device/device-lib/device.c
@@ -2,12 +2,13 @@
 
 #include "device/gpio.h"
 #include "device/ipwm.h"
-#include "device/timer.h"
 
 #include <avr/interrupt.h>
 
-void Device_init(
-  struct Device * self, struct IPwm * bell, struct Gpio * button, struct Gpio * notify)
+bool button_pushed(struct Device * self);
+bool done_ringing(struct Device * self);
+
+void Device_init(struct Device * self, struct IPwm * bell, struct Gpio * button, struct Gpio * notify)
 {
   self->bell = bell;
   self->button = button;
@@ -26,22 +27,44 @@ void Device_init(
 
 void Device_loop(struct Device * self)
 {
-  if(Device_Idle == self->state) {
-    if(Signal_High == Gpio_get_signal(self->button)) {
-      Timer_start(&self->bell_timer, 1000);
-      Timer_start(&self->notify_timer, 20);
-      Pwm_on(self->bell);
-      Gpio_set_signal(self->notify, Signal_High);
-      self->state = Device_Ringing;
-    }
-  } else if(Device_Ringing == self->state) {
-    if(Timer_expired(&self->bell_timer)) {
-      Pwm_off(self->bell);
-      self->state = Device_Idle;
-    }
-    if(Timer_expired(&self->notify_timer))
-    {
-      Gpio_set_signal(self->notify, Signal_Low);
-    }
+  switch(self->state) {
+    default:
+      break;
+
+    case Device_Idle:
+      if(button_pushed(self)) {
+        self->state = Device_Ringing;
+      }
+      break;
+
+    case Device_Ringing:
+      if(done_ringing(self)) {
+        self->state = Device_Idle;
+      }
+      break;
   }
+}
+
+bool done_ringing(struct Device * self) {
+  if(Timer_expired(&self->notify_timer))
+  {
+    Gpio_set_signal(self->notify, Signal_Low);
+  }
+  if(Timer_expired(&self->bell_timer)) {
+    Pwm_off(self->bell);
+    return true;
+  }
+  return false;
+}
+
+bool button_pushed(struct Device * self)
+{
+  if(Signal_High == Gpio_get_signal(self->button)) {
+    Timer_start(&self->bell_timer, 1000);
+    Timer_start(&self->notify_timer, 20);
+    Pwm_on(self->bell);
+    Gpio_set_signal(self->notify, Signal_High);
+    return true;
+  }
+  return false;
 }
