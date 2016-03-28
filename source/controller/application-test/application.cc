3bb2b2af85f73367a37eaf6554b54dbd2821beac
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-28 14:19:50 +0200

    Controller todo list as tests

--- source/controller/application-test/application.cc
+++ source/controller/application-test/application.cc
@@ -5,6 +5,9 @@
 #include <application/icommand.h>
 #include <application/iinterrupt.h>
 
+#include <thread>
+#include <condition_variable>
+
 using namespace ::testing;
 
 namespace Stub {
@@ -30,6 +33,46 @@ public:
 } // namespace Mock
 
 namespace {
+
+TEST(The_application, DISABLED_shall_shutdown_within_10_milliseconds)
+{
+  using namespace std::chrono_literals;
+
+  Controller::Application application;
+
+  std::condition_variable stopped;
+  std::thread application_thread([&]{
+    application.run();
+    stopped.notify_all();
+  });
+
+  application.shutdown();
+
+  std::mutex mutex;
+  std::unique_lock<std::mutex> lock(mutex);
+  ASSERT_THAT(stopped.wait_for(lock, 10ms), Eq(std::cv_status::no_timeout));
+}
+
+TEST(The_application, DISABLED_does_not_shutdown_until_asked_for)
+{
+  using namespace std::chrono_literals;
+
+  Controller::Application application;
+
+  std::condition_variable stopped;
+
+  std::thread application_thread([&]{
+    application.run();
+    stopped.notify_all();
+  });
+
+  std::mutex mutex;
+  std::unique_lock<std::mutex> lock(mutex);
+  ASSERT_THAT(stopped.wait_for(lock, 100ms), Eq(std::cv_status::timeout));
+
+  application.shutdown();
+  stopped.wait(lock);
+}
 
 TEST(The_application, DISABLED_takes_a_picture_when_the_doorbell_rings)
 {
