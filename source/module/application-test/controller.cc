b16b19f2d0e0f162fa58bdafaa3458845b6faf85
Raphael Meyer <raphael.meyer@bbv.ch>
2016-03-29 21:23:49 +0200

    Separate controller further from application

--- source/module/application-test/controller.cc
+++ source/module/application-test/controller.cc
@@ -1,10 +1,16 @@
 #include <gmock/gmock.h>
 
 #include <application/controller.h>
+#include <application/icommand.h>
 
 using namespace testing;
 
 namespace {
+
+class CommandMock : public Module::ICommand {
+public:
+  MOCK_METHOD1(execute, void(std::string const & command));
+};
 
 /*
 class IRequest;
@@ -22,24 +28,22 @@ TEST(The_controller, DISABLED_uses_a_scheduler_handle_a_notification)
   /*
   CommandMock shell;
   SchedulerMock scheduler;
-  Controller controller(shell, scheduler);
+  Controller testee(shell, scheduler);
 
   EXPECT_CALL(scheduler, schedule(_))
     .WillOnce(Return());
   */
 }
 
-TEST(The_controller, DISABLED_takes_a_picture_when_notified_by_the_doorbell)
+TEST(The_controller, takes_a_picture_when_notified_by_the_doorbell)
 {
-  /*
-  CommandMock shell;
-  Scheduler scheduler;
-  Controller controller(shell, scheduler);
+  StrictMock<CommandMock> shell;
+  //Scheduler scheduler;
+  Module::Controller testee(shell);
 
   EXPECT_CALL(shell, execute(StartsWith("raspistill")));
 
-  controller.notify();
-  */
+  testee.notify();
 }
 
 
