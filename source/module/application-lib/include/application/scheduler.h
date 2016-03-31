a603dc297efb512ca38d061c45a9c14e5c86fc64
raphael meyer <raphael.akira.meyer@gmail.com>
2016-03-31 23:16:31 +0200

    Refactor

--- source/module/application-lib/include/application/scheduler.h
+++ source/module/application-lib/include/application/scheduler.h
@@ -10,6 +10,14 @@ public:
   void start();
   virtual std::future<uint32_t> schedule(std::function<uint32_t()> request) override final;
   virtual std::future<std::string> schedule(std::function<std::string()> request) override final;
+
+private:
+  template<typename T>
+  std::future<T> dispatch(std::function<T()> request) {
+    std::promise<T> promise;
+    promise.set_value(request());
+    return promise.get_future();
+  }
 };
 
 }
--- source/module/application-lib/scheduler.cc
+++ source/module/application-lib/scheduler.cc
@@ -6,15 +6,11 @@ void Scheduler::start() {
 }
 
 std::future<uint32_t> Scheduler::schedule(std::function<uint32_t()> request) {
-  std::promise<uint32_t> promise;
-  promise.set_value(request());
-  return promise.get_future();
+  return dispatch(request);
 }
 
 std::future<std::string> Scheduler::schedule(std::function<std::string()> request) {
-  std::promise<std::string> promise;
-  promise.set_value(request());
-  return promise.get_future();
+  return dispatch(request);
 }
 
 
