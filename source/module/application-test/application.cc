0c73a0bebd87b76c1cc98d913ccd95595c97f581
raphael meyer <raphael.akira.meyer@gmail.com>
2016-04-02 11:06:07 +0200

    Add synchronization point to make test robust.

--- source/module/application-test/application.cc
+++ source/module/application-test/application.cc
@@ -8,6 +8,7 @@
 #include <thread>
 
 using namespace testing;
+using namespace std::literals::chrono_literals;
 
 namespace {
 
@@ -33,28 +34,16 @@ TEST(The_application, shall_shutdown_within_10_milliseconds)
 
   testee.shutdown();
 
-  using namespace std::chrono_literals;
-
   std::unique_lock<std::mutex> lock(mutex);
   ASSERT_TRUE(condition.wait_for(lock, 10ms, [&]{ return stopped; }));
 
   application_thread.join();
 }
 
-TEST(The_application, DISABLED_starts_the_controller_on_startup)
-{
-  FAIL();
-}
-
-TEST(The_application, DISABLED_stops_the_controller_on_shutdown)
-{
-  FAIL();
-}
-
-TEST(The_application, DISABLED_takes_a_picture_when_the_doorbell_rings)
+TEST(The_application, takes_a_picture_within_1_second_when_the_doorbell_rings)
 {
   Stub::Interrupt doorbell;
-  Mock::Command shell;
+  StrictMock<Mock::Command> shell;
 
   Module::Application testee(doorbell, shell);
 
@@ -62,9 +51,26 @@ TEST(The_application, DISABLED_takes_a_picture_when_the_doorbell_rings)
     testee.run();
   });
 
-  EXPECT_CALL(shell, execute(StartsWith("raspistill")));
+  std::mutex mutex;
+  std::condition_variable condition;
+  bool picture_taken = false;
+
+  EXPECT_CALL(shell, execute(StartsWith("raspistill")))
+    .WillOnce(InvokeWithoutArgs([&]{
+      {
+        std::lock_guard<std::mutex> lock(mutex);
+        picture_taken = true;
+      }
+      condition.notify_all();
+    }));
 
   doorbell.pulse();
+
+  std::unique_lock<std::mutex> lock(mutex);
+  bool const no_timeout = condition.wait_for(lock, 1s, [&]{ return picture_taken; });
+
+  ASSERT_TRUE(picture_taken);
+  ASSERT_TRUE(no_timeout);
 
   testee.shutdown();
   application_thread.join();
