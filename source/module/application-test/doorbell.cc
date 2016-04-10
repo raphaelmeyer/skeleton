043e0be3bb355f20c09d0901b678ef7092316ed6
Raphael Meyer <raphael.meyer@bbv.ch>
2016-04-10 17:07:18 +0200

    Refactor test

--- source/module/application-test/doorbell.cc
+++ source/module/application-test/doorbell.cc
@@ -86,120 +86,101 @@ private:
   bool _notified;
 };
 
-TEST(The_doorbell, sets_up_the_gpio_for_polling_on_start)
-{
-  PollStub poll;
-  Module::Doorbell testee(poll);
+using namespace std::literals::chrono_literals;
 
+class The_doorbell : public Test {
+protected:
+  The_doorbell()
+    : poll()
+    , testee(poll)
+  {
+  }
+
+  virtual void SetUp() override final {
+
+  }
+
+  virtual void TearDown() override final {
+    testee.stop();
+    poll.trigger(false);
+  }
+
+  PollStub poll;
+  Module::Doorbell testee;
+};
+
+TEST_F(The_doorbell, sets_up_the_gpio_for_polling_on_start)
+{
   testee.start();
 
-  ASSERT_TRUE(poll.is_set_up());
-
-  using namespace std::literals::chrono_literals;
+  // work around
   std::this_thread::sleep_for(10ms);
 
-  testee.stop();
-  poll.trigger(false);
+  ASSERT_TRUE(poll.is_set_up());
 }
 
-TEST(The_doorbell, configures_gpio_17_for_polling)
+TEST_F(The_doorbell, configures_gpio_17_for_polling)
 {
-  PollStub poll;
-  Module::Doorbell testee(poll);
-
   testee.start();
 
   ASSERT_THAT(poll.configured_gpio(), Eq(17));
 
-  using namespace std::literals::chrono_literals;
   std::this_thread::sleep_for(10ms);
-
-  testee.stop();
-  poll.trigger(false);
 }
 
-
-TEST(The_doorbell, notifies_the_subscribers_when_the_button_is_pushed)
+TEST_F(The_doorbell, notifies_the_subscribers_when_the_button_is_pushed)
 {
-  PollStub button;
   SubscriberStub subscriber;
-
-  Module::Doorbell testee(button);
 
   testee.start();
   testee.subscribe(subscriber);
 
-  button.trigger(true);
+  poll.trigger(true);
 
-  using namespace std::literals::chrono_literals;
   ASSERT_TRUE(subscriber.notified_in(10ms));
-
-  testee.stop();
-  button.trigger(false);
 }
 
-TEST(The_doorbell, does_not_notify_the_subscribers_on_a_spurious_wakeup)
+TEST_F(The_doorbell, does_not_notify_the_subscribers_on_a_spurious_wakeup)
 {
-  PollStub button;
   SubscriberStub subscriber;
-
-  Module::Doorbell testee(button);
 
   testee.start();
   testee.subscribe(subscriber);
 
-  button.trigger(false);
+  poll.trigger(false);
 
-  using namespace std::literals::chrono_literals;
   ASSERT_FALSE(subscriber.notified_in(10ms));
-
-  testee.stop();
-  button.trigger(false);
 }
 
-TEST(The_doorbell, notifies_all_subscribers)
+TEST_F(The_doorbell, notifies_all_subscribers)
 {
-  PollStub button;
   SubscriberStub subscriber;
   SubscriberStub another_subscriber;
-
-  Module::Doorbell testee(button);
 
   testee.start();
   testee.subscribe(subscriber);
   testee.subscribe(another_subscriber);
 
-  button.trigger(true);
+  poll.trigger(true);
 
-  using namespace std::literals::chrono_literals;
   ASSERT_TRUE(subscriber.notified_in(10ms));
   ASSERT_TRUE(another_subscriber.notified_in(10ms));
-
   testee.stop();
-  button.trigger(false);
+  poll.trigger(false);
 }
 
-TEST(The_doorbell, notifies_the_subscribers_on_each_interrupt)
+TEST_F(The_doorbell, notifies_the_subscribers_on_each_interrupt)
 {
-  PollStub button;
   SubscriberStub subscriber;
-
-  Module::Doorbell testee(button);
 
   testee.start();
   testee.subscribe(subscriber);
 
-  button.trigger(true);
-
-  using namespace std::literals::chrono_literals;
+  poll.trigger(true);
   ASSERT_TRUE(subscriber.notified_in(10ms));
 
-  button.trigger(true);
-
+  poll.trigger(true);
   ASSERT_TRUE(subscriber.notified_in(10ms));
-
-  testee.stop();
-  button.trigger(false);
 }
 
 }
