ace9983759d2e659c1cc32f820c996c460c841d3
Raphael Meyer <raphael.meyer@bbv.ch>
2016-04-16 23:41:11 +0200

    Target acceptance test

--- module-features-target.sh
+++ module-features-target.sh
@@ -4,5 +4,8 @@ PROJECT_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source ${PROJECT_ROOT}/env-amd64.sh
 
-docker run --rm -t -w ${SRC_GUEST}/module/acceptance-test-target ${VOLUMES} ${CONTAINER} cucumber $@
+RPI=rpi2
+RPI_IP=`getent hosts ${RPI} | awk '{ print $1 }'`
+
+docker run --rm -t --add-host ${RPI}:${RPI_IP} -w ${SRC_GUEST}/module/acceptance-test-target ${VOLUMES} ${CONTAINER} cucumber $@
 
--- source/module/acceptance-test-target/features/step_definitions/steps.rb
+++ source/module/acceptance-test-target/features/step_definitions/steps.rb
@@ -1,18 +1,26 @@
-#require 'rspec/expectations'
-#
-#Given (/^there is no picture yet$/) do
-#  `ssh root@rpi2 rm -f picture.jpg`
-#end
-#
-#When (/^the doorbell rings$/) do
-#  `ssh root@rpi2 echo "5" > "/sys/class/gpio/export`
-#  `ssh root@rpi2 echo "out" > "/sys/class/gpio/gpio5/direction`
-#  `ssh root@rpi2 echo "1" > "/sys/class/gpio/gpio5/value`
-#  # wait 20ms
-#  `ssh root@rpi2 echo "0" > "/sys/class/gpio/gpio5/value`
-#end
-#
-#Then (/^the internet module takes a picture$/) do
-#  @result = `ssh root@rpi2 ls picure.jpg`
-#end
+require 'test/unit/assertions'
+
+def invoke(command)
+  `ssh -oStrictHostKeyChecking=no root@rpi2 #{command}`
+end
+
+Given (/^there is no picture yet$/) do
+  invoke('rm -f picture.jpg')
+end
+
+When (/^the doorbell rings$/) do
+  enabled = invoke('"ls /sys/class/gpio | grep 24"').strip
+  if enabled != 'gpio24'
+    invoke('"echo 24 > /sys/class/gpio/export"')
+  end
+  invoke('"echo out > /sys/class/gpio/gpio24/direction"')
+  invoke('"echo 1 > /sys/class/gpio/gpio24/value"')
+  sleep 0.02
+  invoke('"echo 0 > /sys/class/gpio/gpio24/value"')
+end
+
+Then (/^the internet module takes a picture$/) do
+  result = invoke('ls picture.jpg')
+  assert_equal 'picture.jpg', result.strip
+end
 
