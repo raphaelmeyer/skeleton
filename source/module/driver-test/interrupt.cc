09f3286ba9867ac1b4e2d58bd16cc6aa8eba4c5d
Raphael Meyer <raphael.meyer@bbv.ch>
2016-04-17 21:38:28 +0200

    Crash failing test instead of hanging. Not nice but better.

--- source/module/driver-test/interrupt.cc
+++ source/module/driver-test/interrupt.cc
@@ -12,90 +12,96 @@ using namespace std::literals::chrono_literals;
 namespace
 {
 
-TEST(The_interrupt_polling, blocks_until_a_rising_edge_occurs)
-{
-  std::ofstream enable("/sys/class/gpio/export");
-  enable << "5";
-  enable.close();
+class The_interrupt_polling : public Test {
+protected:
+  The_interrupt_polling()
+  : interrupted()
+  , gpio(26)
+  , poll()
+  {
+  }
 
-  std::ofstream direction("/sys/class/gpio/gpio5/direction");
-  direction << "out";
-  direction.close();
+  virtual void SetUp() override final {
+    std::ofstream enable("/sys/class/gpio/export");
+    enable << "5";
+    enable.close();
 
-  std::ofstream value("/sys/class/gpio/gpio5/value");
-  value << "0" << std::flush;
+    std::ofstream direction("/sys/class/gpio/gpio5/direction");
+    direction << "out";
+    direction.close();
 
-  Module::Gpio::Path gpio(26);
+    std::ofstream value("/sys/class/gpio/gpio5/value");
+    value << "0" << std::flush;
+  }
+
+  Module::Gpio::Path gpio;
   Module::Poll poll;
+  std::future<bool> interrupted;
+};
+
+TEST_F(The_interrupt_polling, blocks_until_a_rising_edge_occurs)
+{
   poll.setup(gpio);
 
-  auto interrupted = std::async(std::launch::async, [&]{
-    return poll.poll();
+  std::promise<bool> poll_result;
+  interrupted = poll_result.get_future();
+
+  std::thread poll_thread([&]{
+    bool const result = poll.poll();
+    poll_result.set_value(result);
   });
 
   ASSERT_THAT(interrupted.wait_for(500ms), Eq(std::future_status::timeout));
 
+  std::ofstream value("/sys/class/gpio/gpio5/value");
   value << "1" << std::flush;
   std::this_thread::sleep_for(20ms);
   value << "0" << std::flush;
 
   ASSERT_THAT(interrupted.wait_for(500ms), Eq(std::future_status::ready));
   ASSERT_TRUE(interrupted.get());
+
+  poll_thread.join();
 }
 
-TEST(The_interrupt_polling, catches_a_rising_edge_that_occured_before_polling)
+TEST_F(The_interrupt_polling, catches_a_rising_edge_that_occured_before_polling)
 {
-  std::ofstream enable("/sys/class/gpio/export");
-  enable << "5";
-  enable.close();
-
-  std::ofstream direction("/sys/class/gpio/gpio5/direction");
-  direction << "out";
-  direction.close();
-
-  std::ofstream value("/sys/class/gpio/gpio5/value");
-  value << "0" << std::flush;
-
-  Module::Gpio::Path gpio(26);
-  Module::Poll poll;
   poll.setup(gpio);
 
+  std::ofstream value("/sys/class/gpio/gpio5/value");
   value << "1" << std::flush;
   std::this_thread::sleep_for(20ms);
   value << "0" << std::flush;
 
-  auto interrupted = std::async(std::launch::async, [&]{
-    return poll.poll();
+  std::promise<bool> poll_result;
+  interrupted = poll_result.get_future();
+
+  std::thread poll_thread([&]{
+    bool const result = poll.poll();
+    poll_result.set_value(result);
   });
 
   ASSERT_THAT(interrupted.wait_for(500ms), Eq(std::future_status::ready));
   ASSERT_TRUE(interrupted.get());
+
+  poll_thread.join();
 }
 
-TEST(The_interrupt_polling, clears_interrupts_on_setup)
+TEST_F(The_interrupt_polling, clears_interrupts_on_setup)
 {
-  std::ofstream enable("/sys/class/gpio/export");
-  enable << "5";
-  enable.close();
-
-  std::ofstream direction("/sys/class/gpio/gpio5/direction");
-  direction << "out";
-  direction.close();
-
   std::ofstream value("/sys/class/gpio/gpio5/value");
-  value << "0" << std::flush;
-
-  Module::Gpio::Path gpio(26);
-  Module::Poll poll;
-
   value << "1" << std::flush;
   std::this_thread::sleep_for(20ms);
   value << "0" << std::flush;
 
   poll.setup(gpio);
 
-  auto interrupted = std::async(std::launch::async, [&]{
-    return poll.poll();
+  std::promise<bool> poll_result;
+  interrupted = poll_result.get_future();
+
+  std::thread poll_thread([&]{
+    bool const result = poll.poll();
+    poll_result.set_value(result);
   });
 
   ASSERT_THAT(interrupted.wait_for(500ms), Eq(std::future_status::timeout));
@@ -106,8 +112,10 @@ TEST(The_interrupt_polling, clears_interrupts_on_setup)
   std::this_thread::sleep_for(20ms);
   value << "0" << std::flush;
 
-  EXPECT_THAT(interrupted.wait_for(500ms), Eq(std::future_status::ready));
-  EXPECT_TRUE(interrupted.get());
+  ASSERT_THAT(interrupted.wait_for(500ms), Eq(std::future_status::ready));
+  ASSERT_TRUE(interrupted.get());
+
+  poll_thread.join();
 }
 
 }
